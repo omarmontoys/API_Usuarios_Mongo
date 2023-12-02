b346ee5bdc1fdf5ffa5f33b8a8888e6f594ac0c5
Omar Montoya <72107693+omarmontoys@users.noreply.github.com>
2023-12-02 12:44:27 -0600

    .

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const routesUser = require("./src/routes/usuario.routes");
 const app = express();
+const cors = require("cors");
 const puerto = process.env.PORT || 3001;
 app.use(express.json());
+app.use(cors());
 app.use("/socios/v1/users", routesUser);
 app.listen(puerto, () => {
 	console.log(
