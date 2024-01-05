bd1eb430e857b09a56abef3f2282f369d729f28d
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2024-01-05 12:04:58 -0600

    :adhesive_bandage: changing the way of retrieving users and adding getAll for testing.

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/usuario.controller");
 
 router.post("/", userController.getAllUser);
 router.get("/:email", userController.getUserByEmail);
+router.get("/", userController.getAll);
 router.post("/add", userController.addUser);
 router.put("/:email", userController.updateUser);
 router.delete("/:email", userController.deleteUser);
